457bbd86e539f21255dd8a015a1c4720fa9f710d
Bodhisattwa Mandal <bodhisattwa.rgkmc@gmail.com>
2025-02-04 23:12:53 +0530

    add pa, te and sort alphabetically

--- app/src/main/java/com/cis/wsreader/helpers/book/BookLanguage.kt
+++ app/src/main/java/com/cis/wsreader/helpers/book/BookLanguage.kt
@@ -14,24 +14,30 @@ sealed class BookLanguage(val name: String, val isoCode: String) {
     data object AllBooks : BookLanguage("All Books", "all")
 
     @Keep
-    data object English : BookLanguage("English", "en")
-
-    @Keep
     data object Bangla : BookLanguage("Bangla", "bn")
-
+    
     @Keep
-    data object French : BookLanguage("French", "fr")
+    data object English : BookLanguage("English", "en")
 
     @Keep
     data object Spanish : BookLanguage("Spanish", "es")
 
     @Keep
+    data object French : BookLanguage("French", "fr")
+
+    @Keep
+    data object Hindi : BookLanguage("Hindi", "hi")
+
+    @Keep
     data object Marathi : BookLanguage("Marathi", "mr")
 
     @Keep
-    data object German : BookLanguage("Hindi", "hi")
+    data object Punjabi : BookLanguage("Punjabi", "pa")
 
     @Keep
-    data object Greek : BookLanguage("Telugu", "te")
+    data object Tamil : BookLanguage("Tamil", "ta")
+
+    @Keep
+    data object Telugu : BookLanguage("Telugu", "te")
 
 }
