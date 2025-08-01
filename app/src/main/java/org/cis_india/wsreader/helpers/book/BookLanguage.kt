645c65b669932d7d6e072941898d4409da9e6548
Bodhisattwa Mandal <bodhisattwa.rgkmc@gmail.com>
2025-08-01 15:20:28 +0530

    Update BookLanguage.kt

--- app/src/main/java/org/cis_india/wsreader/helpers/book/BookLanguage.kt
+++ app/src/main/java/org/cis_india/wsreader/helpers/book/BookLanguage.kt
@@ -14,7 +14,7 @@ sealed class BookLanguage(val name: String, val isoCode: String) {
     data object AllBooks : BookLanguage("All Books", "all")
 
     @Keep
-    data object Bangla : BookLanguage("Assamese", "as")
+    data object Assamese : BookLanguage("Assamese", "as")
 
     @Keep
     data object Bangla : BookLanguage("Bangla", "bn")
