b59a8e736f4da9e9842828ac63e92a531ee00bd5
levy <l.muguro@chem-labs.com>
2025-12-17 19:39:40 +0300

    refactor package name

--- app/src/androidTest/java/org/cis_india/wsreader/ExampleInstrumentedTest.kt
+++ app/src/androidTest/java/org/cis_india/wsreader/ExampleInstrumentedTest.kt
@@ -17,6 +17,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.starry.WikisourceReader", appContext.packageName)
+        assertEquals("org.cis_india.wsreader", appContext.packageName)
     }
 }
