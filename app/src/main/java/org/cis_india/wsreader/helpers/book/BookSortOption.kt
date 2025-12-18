313c3a4f37a2a4777ee0df44d38c69328840c42f
levy <l.muguro@chem-labs.com>
2025-12-19 00:50:14 +0300

    translate sort options, added with english

--- app/src/main/java/org/cis_india/wsreader/helpers/book/BookSortOption.kt
+++ app/src/main/java/org/cis_india/wsreader/helpers/book/BookSortOption.kt
@@ -1,9 +1,11 @@
 package org.cis_india.wsreader.helpers.book
 
 import androidx.annotation.Keep
+import androidx.annotation.StringRes
+import org.cis_india.wsreader.R
 
 @Keep
-sealed class BookSortOption(val displayName: String, val apiValue: String) {
+sealed class BookSortOption(@StringRes val displayName: Int, val apiValue: String) {
 
     companion object {
         fun getAllSortOptions() =
@@ -11,17 +13,17 @@ sealed class BookSortOption(val displayName: String, val apiValue: String) {
     }
 
     @Keep
-    data object PopularityHighToLow : BookSortOption("Popularity: High to Low", "-view_count")
+    data object PopularityHighToLow : BookSortOption(R.string.high_to_low, "-view_count")
 
     @Keep
-    data object PopularityLowToHigh : BookSortOption("Popularity: Low to High", "view_count")
+    data object PopularityLowToHigh : BookSortOption(R.string.low_to_high, "view_count")
 
     @Keep
-    data object TitleAToZ : BookSortOption("Title: A to Z", "title")
+    data object TitleAToZ : BookSortOption(R.string.a_to_z, "title")
 
     @Keep
-    data object TitleZToA : BookSortOption("Title: Z to A", "-title")
+    data object TitleZToA : BookSortOption(R.string.z_to_a, "-title")
 
     @Keep
-    data object Random : BookSortOption("Random", "random")
+    data object Random : BookSortOption(R.string.random, "random")
 }
