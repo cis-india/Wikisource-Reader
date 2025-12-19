1b32a75f020de9eebb25e5c73c15f344ad298b8c
levy <l.muguro@chem-labs.com>
2025-12-19 22:29:10 +0300

    use an easy to read translate id

--- app/src/main/java/org/cis_india/wsreader/helpers/book/BookSortOption.kt
+++ app/src/main/java/org/cis_india/wsreader/helpers/book/BookSortOption.kt
@@ -13,17 +13,17 @@ sealed class BookSortOption(@StringRes val displayName: Int, val apiValue: Strin
     }
 
     @Keep
-    data object PopularityHighToLow : BookSortOption(R.string.high_to_low, "-view_count")
+    data object PopularityHighToLow : BookSortOption(R.string.sort_high_to_low, "-view_count")
 
     @Keep
-    data object PopularityLowToHigh : BookSortOption(R.string.low_to_high, "view_count")
+    data object PopularityLowToHigh : BookSortOption(R.string.sort_low_to_high, "view_count")
 
     @Keep
-    data object TitleAToZ : BookSortOption(R.string.a_to_z, "title")
+    data object TitleAToZ : BookSortOption(R.string.sort_a_to_z, "title")
 
     @Keep
-    data object TitleZToA : BookSortOption(R.string.z_to_a, "-title")
+    data object TitleZToA : BookSortOption(R.string.sort_z_to_a, "-title")
 
     @Keep
-    data object Random : BookSortOption(R.string.random, "random")
+    data object Random : BookSortOption(R.string.sort_random, "random")
 }
