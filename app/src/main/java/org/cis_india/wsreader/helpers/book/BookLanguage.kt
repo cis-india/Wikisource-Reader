6383161046a446d8e5575b05f1a85e1794d43b7b
levy <l.muguro@chem-labs.com>
2026-01-05 09:05:46 +0300

    update language labels to be more descriptive

--- app/src/main/java/org/cis_india/wsreader/helpers/book/BookLanguage.kt
+++ app/src/main/java/org/cis_india/wsreader/helpers/book/BookLanguage.kt
@@ -13,87 +13,87 @@ sealed class BookLanguage(@StringRes val name: Int, val isoCode: String) {
     }
 
     @Keep
-    data object AllBooks : BookLanguage(R.string.language_name_all_books, "all")
+    data object AllBooks : BookLanguage(R.string.all_books_language_lebel, "all")
 
     @Keep
-    data object Arabic : BookLanguage(R.string.language_name_arabic, "ar")
+    data object Arabic : BookLanguage(R.string.arabic_language_label, "ar")
 
     @Keep
-    data object Assamese : BookLanguage(R.string.language_name_assamese, "as")
+    data object Assamese : BookLanguage(R.string.assamese_language_label, "as")
 
     @Keep
-    data object Bangla : BookLanguage(R.string.language_name_bangla, "bn")
+    data object Bangla : BookLanguage(R.string.bangla_language_label, "bn")
 
     @Keep
-    data object Catalan : BookLanguage(R.string.language_name_catalan, "ca")
+    data object Catalan : BookLanguage(R.string.catalan_language_label, "ca")
     
     @Keep
-    data object Czech : BookLanguage(R.string.language_name_czech, "cs")
+    data object Czech : BookLanguage(R.string.czech_language_label, "cs")
 
     @Keep
-    data object Danish : BookLanguage(R.string.language_name_danish, "da")
+    data object Danish : BookLanguage(R.string.danish_language_lebel, "da")
     
     @Keep
-    data object English : BookLanguage(R.string.language_name_english, "en")
+    data object English : BookLanguage(R.string.english_language_label, "en")
 
     @Keep
-    data object Spanish : BookLanguage(R.string.language_name_spanish, "es")
+    data object Spanish : BookLanguage(R.string.spanish_language_label, "es")
 
     @Keep
-    data object French : BookLanguage(R.string.language_name_french, "fr")
+    data object French : BookLanguage(R.string.french_language_label, "fr")
 
     @Keep
-    data object Gujarati : BookLanguage(R.string.language_name_gujarati, "gu")
+    data object Gujarati : BookLanguage(R.string.gujarati_language_label, "gu")
 
     @Keep
-    data object Hindi : BookLanguage(R.string.language_name_hindi, "hi")
+    data object Hindi : BookLanguage(R.string.hindi_language_label, "hi")
 
     @Keep
-    data object Indonesian : BookLanguage(R.string.language_name_indonesian, "id")
+    data object Indonesian : BookLanguage(R.string.indonesian_language_label, "id")
 
     @Keep
-    data object Italian : BookLanguage(R.string.language_name_italian, "it")
+    data object Italian : BookLanguage(R.string.italian_language_label, "it")
 
     @Keep
-    data object Javanese : BookLanguage(R.string.language_name_javanese, "jv")
+    data object Javanese : BookLanguage(R.string.javanese_language_label, "jv")
 
     @Keep
-    data object Kannada : BookLanguage(R.string.language_name_kannada, "kn")
+    data object Kannada : BookLanguage(R.string.kannada_language_label, "kn")
 
     @Keep
-    data object Malayalam : BookLanguage(R.string.language_name_malayalam, "ml")
+    data object Malayalam : BookLanguage(R.string.malayalam_language_label, "ml")
 
     @Keep
-    data object Marathi : BookLanguage(R.string.language_name_marathi, "mr")
+    data object Marathi : BookLanguage(R.string.marathi_language_label, "mr")
 
     @Keep
-    data object Malay : BookLanguage(R.string.language_name_malay, "ms")
+    data object Malay : BookLanguage(R.string.malay_language_label, "ms")
 
     @Keep
-    data object Polish : BookLanguage(R.string.language_name_polish, "pl")
+    data object Polish : BookLanguage(R.string.polish_language_label, "pl")
 
     @Keep
-    data object Punjabi : BookLanguage(R.string.language_name_punjabi, "pa")
+    data object Punjabi : BookLanguage(R.string.punjabi_language_label, "pa")
 
     @Keep
-    data object Sundanese : BookLanguage(R.string.language_name_sundanese, "su")
+    data object Sundanese : BookLanguage(R.string.sundanese_language_label, "su")
     
     @Keep
-    data object Swedish : BookLanguage(R.string.language_name_swedish, "sv")
+    data object Swedish : BookLanguage(R.string.swedish_language_label, "sv")
 
     @Keep
-    data object Tamil : BookLanguage(R.string.language_name_tamil, "ta")
+    data object Tamil : BookLanguage(R.string.tamil_language_label, "ta")
 
     @Keep
-    data object Telugu : BookLanguage(R.string.language_name_telugu, "te")
+    data object Telugu : BookLanguage(R.string.telugu_language_label, "te")
 
     @Keep
-    data object Tulu : BookLanguage(R.string.language_name_tulu, "tcy")
+    data object Tulu : BookLanguage(R.string.tulu_language_label, "tcy")
 
     @Keep
-    data object Ukrainian : BookLanguage(R.string.language_name_ukrainian, "uk")
+    data object Ukrainian : BookLanguage(R.string.ukrainian_language_label, "uk")
 
     @Keep
-    data object Vietnamese : BookLanguage(R.string.language_name_vietnamese, "vi")
+    data object Vietnamese : BookLanguage(R.string.vietnamese_language_label, "vi")
 
 }
