6a6f3a0bab7d65bf6ce656e9c4fd8da45587d26a
Bodhisattwa <bodhisattwa.rgkmc@gmail.com>
2026-08-10 19:18:18 +0530

    Update BookLanguage.kt

--- app/src/main/java/org/cis_india/wsreader/helpers/book/BookLanguage.kt
+++ app/src/main/java/org/cis_india/wsreader/helpers/book/BookLanguage.kt
@@ -85,7 +85,7 @@ sealed class BookLanguage(@StringRes val name: Int, val isoCode: String) {
     data object Polish : BookLanguage(R.string.polish_language_label, "pl")
 
     @Keep
-    data object Portuguese : BookLanguage(R.string.polish_language_label, "pt")
+    data object Portuguese : BookLanguage(R.string.portuguese _language_label, "pt")
 
     @Keep
     data object Punjabi : BookLanguage(R.string.punjabi_language_label, "pa")
